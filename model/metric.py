2de9b02c0657c50101d579cc27d820dc54610289
Weina Jin <drweinajin@gmail.com>
2019-02-19 16:08:24 -0800

    metrics

--- model/metric.py
+++ model/metric.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def my_metric(output, target):
+def overal_acc(output, target):
     with torch.no_grad():
         pred = torch.argmax(output, dim=1)
         assert pred.shape[0] == len(target)
@@ -9,7 +9,7 @@ def my_metric(output, target):
         correct += torch.sum(pred == target).item()
     return correct / len(target)
 
-def my_metric2(output, target, k=3):
+def topk_acc(output, target, k=3):
     with torch.no_grad():
         pred = torch.topk(output, k, dim=1)[1]
         assert pred.shape[0] == len(target)
@@ -17,3 +17,7 @@ def my_metric2(output, target, k=3):
         for i in range(k):
             correct += torch.sum(pred[:, i] == target).item()
     return correct / len(target)
+
+
+def class_acc(output, target):
+    return
