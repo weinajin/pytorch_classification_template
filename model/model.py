d2bd46ea06128fd02948baa93b72d86e461ecdf9
Weina Jin <drweinajin@gmail.com>
2019-03-17 15:33:59 -0700

    modify activation.py

--- model/model.py
+++ model/model.py
@@ -11,12 +11,36 @@ class AlexNet(BaseModel):
         super(AlexNet, self).__init__()
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        
         self.conv2 = nn.Conv2d(64, 192, kernel_size=5, padding=2)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
+        
         self.conv3 = nn.Conv2d(192, 384, kernel_size=3, padding=1)
+        self.relu3 = nn.ReLU(inplace=True)
+        
         self.conv4 = nn.Conv2d(384, 256, kernel_size=3, padding=1)
+        self.relu4 = nn.ReLU(inplace=True)
+        
         self.conv5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-
+        self.relu5 = nn.ReLU(inplace=True)
+        self.pool5 = nn.MaxPool2d(kernel_size=3, stride=2)
+        
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        
+#         # fc layer
+#         self.dropout1 = nn.Dropout()
+#         self.fc1 = nn.Linear(256 * 6 * 6, 4096)
+#         self.relu6 = nn.ReLU(inplace=True)
+        
+#         self.dropout2 = nn.Dropout()
+#         self.fc2 = nn.Linear(4096, 4096)
+#         self.relu7 = nn.ReLU(inplace=True)
+        
+#         self.fc3 = nn.Linear(4096, num_classes)
+        
         self.classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
@@ -29,22 +53,37 @@ class AlexNet(BaseModel):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = F.max_pool2d(F.relu(x, True), kernel_size=3, stride=2)
+        x = self.relu1(x)
+        x = self.pool1(x)
 
         x = self.conv2(x)
-        x = F.max_pool2d(F.relu(x, True), kernel_size=3, stride=2)
+        x = self.relu2(x)
+        x = self.pool2(x)
 
         x = self.conv3(x)
-        x = F.relu(x, True)
+        x = self.relu3(x)
 
         x = self.conv4(x)
-        x = F.relu(x, True)
+        x = self.relu4(x)
 
         x = self.conv5(x)
-        x = F.max_pool2d(F.relu(x, True), kernel_size=3, stride=2)
+        x = self.relu5(x)
+        x = self.pool5(x)
 
         x = self.avgpool(x)
         x = x.view(x.size(0), 256 * 6 * 6)
+        
+#         # fc layers
+#         x = self.droput1(x)
+#         x = self.fc1(x)
+#         x = self.relu6(x)
+        
+#         x = self.dropout2(x)
+#         x = self.fc2(x)
+#         x = self.relu7(x)
+        
+#         x = self.fc3(x)
+
         x = self.classifier(x)
         return x
 
---
+++ preprocess_ISIC.py
@@ -0,0 +1,107 @@
+# to make the image dir correct, the preprocess should be put at the same dir of the main experiment scropt, i.e. under the root.
+import csv
+import os
+import os.path
+import glob
+from PIL import Image
+import numpy as np
+import torch
+
+# *** Val - melanoma: 30, nevus: 78, sk: 42.
+# *** Test - melanoma: 117, nevus: 393, sk: 90.
+
+# one time preprocess data
+def preprocess_ISICdata_2017(root_dir = 'skindata/', seg_dir='Train_Lesion'):
+    print('*** pre-processing data ...\n')
+    # training data
+    melanoma = glob.glob(os.path.join(root_dir, 'Train', 'melanoma', '*.jpg')); melanoma.sort()
+    nevus    = glob.glob(os.path.join(root_dir, 'Train', 'nevus', '*.jpg')); nevus.sort()
+    sk       = glob.glob(os.path.join(root_dir, 'Train', 'seborrheic_keratosis', '*.jpg')); sk.sort()
+    melanoma_seg = glob.glob(os.path.join(root_dir, seg_dir, 'melanoma', '*.png')); melanoma_seg.sort()
+    nevus_seg    = glob.glob(os.path.join(root_dir, seg_dir, 'nevus', '*.png')); nevus_seg.sort()
+    sk_seg       = glob.glob(os.path.join(root_dir, seg_dir, 'seborrheic_keratosis', '*.png')); sk_seg.sort()
+    with open(root_dir+'train.csv', 'wt', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for k in range(len(melanoma)):
+            filename = melanoma[k]
+            filename_seg = melanoma_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['1'])
+        for k in range(len(nevus)):
+            filename = nevus[k]
+            filename_seg = nevus_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+        for k in range(len(sk)):
+            filename = sk[k]
+            filename_seg = sk_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+    # training data oversample
+    melanoma = glob.glob(os.path.join(root_dir, 'Train', 'melanoma', '*.jpg')); melanoma.sort()
+    nevus    = glob.glob(os.path.join(root_dir, 'Train', 'nevus', '*.jpg')); nevus.sort()
+    sk       = glob.glob(os.path.join(root_dir, 'Train', 'seborrheic_keratosis', '*.jpg')); sk.sort()
+    melanoma_seg = glob.glob(os.path.join(root_dir, seg_dir, 'melanoma', '*.png')); melanoma_seg.sort()
+    nevus_seg    = glob.glob(os.path.join(root_dir, seg_dir, 'nevus', '*.png')); nevus_seg.sort()
+    sk_seg       = glob.glob(os.path.join(root_dir, seg_dir, 'seborrheic_keratosis', '*.png')); sk_seg.sort()
+    with open(root_dir+'train_oversample.csv', 'wt', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for i in range(4):
+            for k in range(len(melanoma)):
+                filename = melanoma[k]
+                filename_seg = melanoma_seg[k]
+                writer.writerow([filename] + [filename_seg] + ['1'])
+        for k in range(len(nevus)):
+            filename = nevus[k]
+            filename_seg = nevus_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+        for k in range(len(sk)):
+            filename = sk[k]
+            filename_seg = sk_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+    # val data
+    melanoma = glob.glob(os.path.join(root_dir, 'Val', 'melanoma', '*.jpg')); melanoma.sort()
+    nevus    = glob.glob(os.path.join(root_dir, 'Val', 'nevus', '*.jpg')); nevus.sort()
+    sk       = glob.glob(os.path.join(root_dir, 'Val', 'seborrheic_keratosis', '*.jpg')); sk.sort()
+    print('*** Val - melanoma: {}, nevus: {}, sk: {}.'.format(len(melanoma), len(nevus), len(sk))) # 
+    #### segmentation of val data is not used! ######
+    melanoma_seg = glob.glob(os.path.join(root_dir, 'Val', 'melanoma', '*.jpg')); melanoma_seg.sort()
+    nevus_seg    = glob.glob(os.path.join(root_dir, 'Val', 'nevus', '*.jpg')); nevus_seg.sort()
+    sk_seg       = glob.glob(os.path.join(root_dir, 'Val', 'seborrheic_keratosis', '*.jpg')); sk_seg.sort()
+    with open(root_dir+'val.csv', 'wt', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for k in range(len(melanoma)):
+            filename = melanoma[k]
+            filename_seg = melanoma_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['1'])
+        for k in range(len(nevus)):
+            filename = nevus[k]
+            filename_seg = nevus_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+        for k in range(len(sk)):
+            filename = sk[k]
+            filename_seg = sk_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+    # test data
+    melanoma = glob.glob(os.path.join(root_dir, 'Test', 'melanoma', '*.jpg')); melanoma.sort()
+    nevus    = glob.glob(os.path.join(root_dir, 'Test', 'nevus', '*.jpg')); nevus.sort()
+    sk       = glob.glob(os.path.join(root_dir, 'Test', 'seborrheic_keratosis', '*.jpg')); sk.sort()
+    print('*** Test - melanoma: {}, nevus: {}, sk: {}.'.format(len(melanoma), len(nevus), len(sk)))
+    melanoma_seg = glob.glob(os.path.join(root_dir, 'Test_Lesion', 'melanoma', '*.png')); melanoma_seg.sort()
+    nevus_seg    = glob.glob(os.path.join(root_dir, 'Test_Lesion', 'nevus', '*.png')); nevus_seg.sort()
+    sk_seg       = glob.glob(os.path.join(root_dir, 'Test_Lesion', 'seborrheic_keratosis', '*.png')); sk_seg.sort()
+    with open(root_dir+'test.csv', 'wt', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for k in range(len(melanoma)):
+            filename = melanoma[k]
+            filename_seg = None#melanoma_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['1'])
+        for k in range(len(nevus)):
+            filename = nevus[k]
+            filename_seg = None# nevus_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+        for k in range(len(sk)):
+            filename = sk[k]
+            filename_seg = None# sk_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+
+if __name__ == '__main__':
+    # # one time preprocess of ISIC 2017 data
+    preprocess_ISICdata_2017()
