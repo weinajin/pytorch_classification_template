e64696da337ee71dd90dca90d414a54ecd7d6c71
Weina Jin <drweinajin@gmail.com>
2019-03-17 15:33:59 -0700

    modify activation.py

--- activation.py
+++ activation.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import copy
 
 import pickle
+import json
 
 class ExtractActivation():
     '''
@@ -24,16 +25,21 @@ class ExtractActivation():
         # setup data_loader instances
         self.data_loader = getattr(module_data, config['data_loader']['type'])(
             config['data_loader']['args']['data_dir'],
-            batch_size= 512 , 
+            batch_size= 16 , 
             shuffle=False,
             validation_split=0.0,
             training=False,
             num_workers=2
         )
-#        print(next(iter(self.data_loader)).shape)
+
         # build model architecture
         self.model = get_instance(module_arch, 'arch', config)
-        self.model.summary()
+        
+        # remove all container/sequential layers to make extract actv map easy
+        self.all_layers = []
+        self.remove_sequential(self.model)
+#         print(self.all_layers)
+#         self.model.summary()
 
         # get function handles of loss and metrics
         self.loss_fn = getattr(module_loss, config['loss'])
@@ -50,29 +56,42 @@ class ExtractActivation():
         # prepare model for testing
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.model = self.model.to(self.device)
-        
         self.model.eval()
 
-        # get nb of neurons in each layer
-        self.neuron_nb = {} # a dict {layer_0: num_neuron} record the number of neurons in each layer
-        for a, m in self.model._modules.items():
-            try:
-                self.neuron_nb[a] = m.out_channels # conv layers
-            except:
-                self.neuron_nb[a] = m.out_features # linear layers
-        
-        # record activation map for neurons in each layer
+        # attach hook to conv+relu, or fc+relu, or fc layers
+        for i in range(len(self.all_layers)):  
+            current_l = self.all_layers[i]
+            prev_l = self.all_layers[i-1]
+            if isinstance(current_l, torch.nn.modules.activation.ReLU):
+                if isinstance(prev_l, torch.nn.modules.conv.Conv2d) \
+                or isinstance(prev_l, torch.nn.modules.linear.Linear):
+                    # register hook on rectified activations
+                    current_l.register_forward_hook(self.record_activation_map) 
+                elif isinstance(current_l, torch.nn.modules.linear.Linear):
+                    current_l.register_forward_hook(self.record_activation_map)                     
+#             print(type(self.all_layers[i]))        
+    
+        # initialize the global variabel to record prediction and gt, and actv map 
         self.activation_map = dict()
-        for m in self.model.children():
-            m.register_forward_hook(self.record_activation_map)
-        # initialize the global variabel to record prediction and gt, clear the record for class initilization 
         self.pred = None
         self.gt = None
         self.total_metrics = dict()
+        
         # record the layer seq {module: i}
-        self.module_seq = dict()
+        self.module_seq = dict()  # sequence of the actv
         self.i = 0
         self.map_shape = []
+
+    def remove_sequential(self, network):
+        # source: https://discuss.pytorch.org/t/module-children-vs-module-modules/4551/5
+        for layer in network.children():
+            if type(layer) == torch.nn.modules.container.Sequential: 
+                # if sequential layer, apply recursively to layers in sequential layer
+                self.remove_sequential(layer)
+            if list(layer.children()) == []: # if leaf node, add it to list
+                self.all_layers.append(layer)
+        
+            
     def record_activation_map(self, module, ipt, opt):
         '''
         record activation map for each layer (module)
@@ -86,26 +105,29 @@ class ExtractActivation():
             self.map_shape.append(opt.shape)
             self.activation_map[self.module_seq[module]] = torch.Tensor()
             self.activation_map[self.module_seq[module]] = torch.cat((self.activation_map[self.module_seq[module]], opt.cpu()))
-        print('recorded activation map shape:', self.activation_map[self.module_seq[module]].shape, opt.shape)
+        print('*** recorded actv map shape:', self.activation_map[self.module_seq[module]].shape, opt.shape)
 
+        
+        
     def evaluate(self):
         total_loss = 0.0
         scalar_metrics = torch.zeros(len(self.metric_fns))
-        class_metrics = {met.__name__: [] for met in self.class_metric_fns} #torch.zeros(len(self.metric_fns))
+        class_metrics = {met.__name__: [] for met in self.class_metric_fns} 
         # record the original output with gt
         self.gt = torch.LongTensor().to(self.device)
         self.pred = torch.FloatTensor().to(self.device)
         with torch.no_grad():
-            for i, (data, target) in enumerate(tqdm(self.data_loader)):
-                data, target = data.to(self.device), target.to(self.device)
-                output = self.model(data)
+            for i, data in enumerate(tqdm(self.data_loader)):
+                inputs, target = data['image'], data['label']
+                inputs, target  = inputs.to(self.device), target.to(self.device)
+                output = self.model(inputs)
                 # record by concatenate the gt and output
                 self.gt = torch.cat((self.gt, target), dim =0)
                 self.pred = torch.cat((self.pred, output.data))
 
                 # computing loss, metrics on test set
                 loss = self.loss_fn(output, target)
-                batch_size = data.shape[0]
+                batch_size = inputs.shape[0]
                 total_loss += loss.item() * batch_size
 
             # given the gt and output, calculate the eval metrics for the whole val set
@@ -121,8 +143,6 @@ class ExtractActivation():
         print("evaluation metrics", self.total_metrics)
         return self.total_metrics
 
-    def get_neuron_nb(self):
-        return self.neuron_nb
     
     def get_gt(self):
         return self.gt
@@ -150,14 +170,22 @@ if __name__ == '__main__':
                            help='path to latest checkpoint (default: None)')
     parser.add_argument('-d', '--device', default=None, type=str,
                            help='indices of GPUs to enable (default: all)')
+    parser.add_argument('-c', '--config', default=None, type=str, help='config file path (default: None)')
 
     args = parser.parse_args()
-
-    if args.resume:
+    if args.config:
+        # load config file
+        config = json.load(open(args.config))
+    elif args.resume:
         config = torch.load(args.resume)['config']
+    else:
+        raise AssertionError("Configuration file need to be specified. Add '-c config.json', for example.")
+    if args.config and args.resume:   
+        print('*** Loading config file: {}, checkpoint file: {} ***'.format(args.config, args.resume))
+    
     if args.device:
         os.environ["CUDA_VISIBLE_DEVICES"]=args.device
-
+        
     extract = ExtractActivation(config, args.resume) 
     extract.evaluate()
     extract.save_data('./saved/')
--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -3,105 +3,11 @@ from torchvision import datasets, transforms
 from base import BaseDataLoader
 
 # for ISIC data
-import csv
-import os
-import os.path
-from PIL import Image
-import glob
-import numpy as np
-import torch
-import torch.utils.data as udata
-from transforms import *
 
-# one time preprocess data
-def preprocess_ISICdata_2017(root_dir, seg_dir='Train_Lesion'):
-    print('pre-processing data ...\n')
-    # training data
-    melanoma = glob.glob(os.path.join(root_dir, 'Train', 'melanoma', '*.jpg')); melanoma.sort()
-    nevus    = glob.glob(os.path.join(root_dir, 'Train', 'nevus', '*.jpg')); nevus.sort()
-    sk       = glob.glob(os.path.join(root_dir, 'Train', 'seborrheic_keratosis', '*.jpg')); sk.sort()
-    melanoma_seg = glob.glob(os.path.join(root_dir, seg_dir, 'melanoma', '*.png')); melanoma_seg.sort()
-    nevus_seg    = glob.glob(os.path.join(root_dir, seg_dir, 'nevus', '*.png')); nevus_seg.sort()
-    sk_seg       = glob.glob(os.path.join(root_dir, seg_dir, 'seborrheic_keratosis', '*.png')); sk_seg.sort()
-    with open(rood_dir+'train.csv', 'wt', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
-        for k in range(len(melanoma)):
-            filename = melanoma[k]
-            filename_seg = melanoma_seg[k]
-            writer.writerow([filename] + [filename_seg] + ['1'])
-        for k in range(len(nevus)):
-            filename = nevus[k]
-            filename_seg = nevus_seg[k]
-            writer.writerow([filename] + [filename_seg] + ['0'])
-        for k in range(len(sk)):
-            filename = sk[k]
-            filename_seg = sk_seg[k]
-            writer.writerow([filename] + [filename_seg] + ['0'])
-    # training data oversample
-    melanoma = glob.glob(os.path.join(root_dir, 'Train', 'melanoma', '*.jpg')); melanoma.sort()
-    nevus    = glob.glob(os.path.join(root_dir, 'Train', 'nevus', '*.jpg')); nevus.sort()
-    sk       = glob.glob(os.path.join(root_dir, 'Train', 'seborrheic_keratosis', '*.jpg')); sk.sort()
-    melanoma_seg = glob.glob(os.path.join(root_dir, seg_dir, 'melanoma', '*.png')); melanoma_seg.sort()
-    nevus_seg    = glob.glob(os.path.join(root_dir, seg_dir, 'nevus', '*.png')); nevus_seg.sort()
-    sk_seg       = glob.glob(os.path.join(root_dir, seg_dir, 'seborrheic_keratosis', '*.png')); sk_seg.sort()
-    with open(rood_dir+'train_oversample.csv', 'wt', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
-        for i in range(4):
-            for k in range(len(melanoma)):
-                filename = melanoma[k]
-                filename_seg = melanoma_seg[k]
-                writer.writerow([filename] + [filename_seg] + ['1'])
-        for k in range(len(nevus)):
-            filename = nevus[k]
-            filename_seg = nevus_seg[k]
-            writer.writerow([filename] + [filename_seg] + ['0'])
-        for k in range(len(sk)):
-            filename = sk[k]
-            filename_seg = sk_seg[k]
-            writer.writerow([filename] + [filename_seg] + ['0'])
-    # val data
-    melanoma = glob.glob(os.path.join(root_dir, 'Val', 'melanoma', '*.jpg')); melanoma.sort()
-    nevus    = glob.glob(os.path.join(root_dir, 'Val', 'nevus', '*.jpg')); nevus.sort()
-    sk       = glob.glob(os.path.join(root_dir, 'Val', 'seborrheic_keratosis', '*.jpg')); sk.sort()
-    #### segmentation of val data is not used! ######
-    melanoma_seg = glob.glob(os.path.join(root_dir, 'Val', 'melanoma', '*.jpg')); melanoma_seg.sort()
-    nevus_seg    = glob.glob(os.path.join(root_dir, 'Val', 'nevus', '*.jpg')); nevus_seg.sort()
-    sk_seg       = glob.glob(os.path.join(root_dir, 'Val', 'seborrheic_keratosis', '*.jpg')); sk_seg.sort()
-    with open(rood_dir+'val.csv', 'wt', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
-        for k in range(len(melanoma)):
-            filename = melanoma[k]
-            filename_seg = melanoma_seg[k]
-            writer.writerow([filename] + [filename_seg] + ['1'])
-        for k in range(len(nevus)):
-            filename = nevus[k]
-            filename_seg = nevus_seg[k]
-            writer.writerow([filename] + [filename_seg] + ['0'])
-        for k in range(len(sk)):
-            filename = sk[k]
-            filename_seg = sk_seg[k]
-            writer.writerow([filename] + [filename_seg] + ['0'])
-    # test data
-    melanoma = glob.glob(os.path.join(root_dir, 'Test', 'melanoma', '*.jpg')); melanoma.sort()
-    nevus    = glob.glob(os.path.join(root_dir, 'Test', 'nevus', '*.jpg')); nevus.sort()
-    sk       = glob.glob(os.path.join(root_dir, 'Test', 'seborrheic_keratosis', '*.jpg')); sk.sort()
-    melanoma_seg = glob.glob(os.path.join(root_dir, 'Test_Lesion', 'melanoma', '*.png')); melanoma_seg.sort()
-    nevus_seg    = glob.glob(os.path.join(root_dir, 'Test_Lesion', 'nevus', '*.png')); nevus_seg.sort()
-    sk_seg       = glob.glob(os.path.join(root_dir, 'Test_Lesion', 'seborrheic_keratosis', '*.png')); sk_seg.sort()
-    with open(rood_dir+'test.csv', 'wt', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
-        for k in range(len(melanoma)):
-            filename = melanoma[k]
-            filename_seg = melanoma_seg[k]
-            writer.writerow([filename] + [filename_seg] + ['1'])
-        for k in range(len(nevus)):
-            filename = nevus[k]
-            filename_seg = nevus_seg[k]
-            writer.writerow([filename] + [filename_seg] + ['0'])
-        for k in range(len(sk)):
-            filename = sk[k]
-            filename_seg = sk_seg[k]
-            writer.writerow([filename] + [filename_seg] + ['0'])
+
+import torch.utils.data as udata
+from .img_transform import *
+import csv
 
 class ISICDataset(udata.Dataset):
     def __init__(self, csv_file, transform=None):
@@ -125,7 +31,7 @@ class ISICDataset(udata.Dataset):
 
 
 class ISICDataLoader(BaseDataLoader):
-    def __init__(self, data_dir, batch_size, shuffle, validation_split, num_workers, training=False):
+    def __init__(self, csv_file, batch_size, shuffle, validation_split, num_workers, training=False):
         normalize = Normalize((0.6820, 0.5312, 0.4736), (0.0840, 0.1140, 0.1282))
         trsfm = transforms.Compose([
             RatioCenterCrop(0.8),
@@ -134,8 +40,8 @@ class ISICDataLoader(BaseDataLoader):
             ToTensor(),
             normalize
         ])
-        self.data_dir = data_dir
-        self.dataset = ISICDataset(self.data_dir, train=training, download=True, transform=trsfm)
+        self.csv_file = csv_file
+        self.dataset = ISICDataset(self.csv_file, transform=trsfm)
         super(ISICDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
         
         
@@ -155,7 +61,3 @@ class MnistDataLoader(BaseDataLoader):
         super(MnistDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
         
-if __name__ == '__main__':
-    # # one time preprocess of ISIC 2017 data
-    root_dir = 'skindata/'
-    preprocess_ISICdata_2017(root_dir)
